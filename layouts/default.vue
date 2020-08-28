08d0a7c01ea7deeb78dfbd6f7d2da10efa7d3c56
ZTF666 <ZTF666@protonmail.ch>
2020-08-28 10:25:15 +0100

    :alien:

---
+++ layouts/default.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-app :dark="setTheme">
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="primary secondary--text"
+      class="hidden-xs-only"
+    >
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2 img"
+        contain
+        :src="logo"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title v-text="title" />
+
+      <v-btn text>
+        <v-switch v-model="goDark" color="dark" :label="icon" class="mt-5"></v-switch>
+      </v-btn>
+      <v-spacer />
+      <v-btn text class="secondary--text" nuxt to="/">Home</v-btn>
+      <v-btn text class="secondary--text mr-5" nuxt to="/inspire">About</v-btn>
+    </v-app-bar>
+
+    <v-app-bar :clipped-left="clipped" fixed app color="primary" class="hidden-sm-and-up">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2 img"
+        contain
+        :src="logo"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title v-text="title" class="secondary--text" />
+      <v-spacer></v-spacer>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <!-- <v-list-item> -->
+          <v-list-item-content>
+            <v-btn text>
+              <v-switch v-model="goDark" color="secondary" :label="icon" class="mb-5"></v-switch>
+            </v-btn>
+
+            <!-- <v-spacer></v-spacer> -->
+            <v-btn text class="secondary--text" nuxt to="/">Home</v-btn>
+            <v-btn text class="secondary--text mr-5" nuxt to="/inspire">About</v-btn>
+          </v-list-item-content>
+
+          <!-- </v-list-item> -->
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-main :class="BgCol">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app color="primary secondary--text">
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      title: 'Vuetify.js',
+      icon: '🌚',
+      goDark: true,
+      BgCol: 'bg',
+      logo:
+        'https://avatars0.githubusercontent.com/u/32502988?s=460&u=5a6935d6381f2f23e3901dfd6f2b60eaf46c9c53&v=4',
+    }
+  },
+  computed: {
+    setTheme() {
+      if (this.goDark === true) {
+        this.$vuetify.theme.dark = true
+        this.icon = '🌚'
+        this.BgCol = 'bg'
+      } else {
+        this.$vuetify.theme.dark = false
+        this.icon = '🌞'
+        this.BgCol = 'bgl'
+      }
+    },
+  },
+}
+</script>
+<style lang="scss">
+.img {
+  border-radius: 100px;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,110 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'universal',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          // custom
+          primary: '#293556',
+          secondary: '#DD4470',
+          success: '#39AEA9',
+          accent: '#FE72A9',
+          pinkish: '#FF7BBF',
+        },
+        light: {
+          primary: '#3f51b5',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c',
+          // primary: '#313866',
+          // secondary: '#50409A',
+          // accent: '#964EC2',
+          // error: '#FF7BBF',
+        },
+      }
+    }
+  },
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {}
+}
