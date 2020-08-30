2d8c5c992d5a454b0d93fa404c256fcbb0ed27ee
ZTF666 <ZTF666@protonmail.ch>
2020-08-30 10:07:09 +0100

    :alien:

--- components/gallery.vue
+++ components/gallery.vue
@@ -9,17 +9,17 @@
           </v-card-text>
         </center>
         <VuePureLightbox
-          thumbnail="https://ztfportfolio.web.app/_nuxt/img/me.1fafa89.png"
+          thumbnail="https://avatars0.githubusercontent.com/u/32502988?s=460&u=5a6935d6381f2f23e3901dfd6f2b60eaf46c9c53&v=4"
           class="gg"
           :images="[
               {
-                link: 'https://ztfportfolio.web.app/_nuxt/img/me.1fafa89.png',
-                alt: 'Cat 2',
+                link: 'https://avatars0.githubusercontent.com/u/32502988?s=460&u=5a6935d6381f2f23e3901dfd6f2b60eaf46c9c53&v=4',
+                alt: 'Me',
               },
               {
                 link:
                   'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FigHeO82Y648%2Fmaxresdefault.jpg&f=1&nofb=1',
-                alt: 'Cat 2',
+                alt: 'Cat',
               },
             ]"
         >
